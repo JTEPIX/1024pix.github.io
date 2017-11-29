6b9a622ce24bf7024a600f24be95ef8b06debb52
Benoit <benoit@air-de-benoit.home>
2017-11-29 15:12:27 +0100

    Ajout lib utils.js

---
+++ js/utils.js
@@ -0,0 +1,34 @@
+
+function getBase() {
+  var path = window.location.pathname;
+  return path.split("/").pop();
+}
+
+function decode(value, base) {
+  var result = "";
+  if (typeof base === "undefined") {
+    base = getBase();
+  }
+  i = 0;
+  j = 0;
+  while (i<value.length) {
+    result = result + String.fromCharCode(parseInt(value.substring(i,i+2),16) ^ base.charCodeAt(j));
+    i+=2;
+    j = (j+1) % base.length;
+  }
+  return result;
+}
+
+function init() {
+  var value = window.location.hash.substr(1);
+  if (value === "config" && window.self !== window.top && typeof window.secret !== "undefined") {
+    window.top.postMessage({secret:window.secret}, "*");
+  }
+}
+
+function getWord() {
+  var value = window.location.hash.substr(1);
+  return decode(decode(value, window.secret));
+}
+
+document.addEventListener("DOMContentLoaded", init);
